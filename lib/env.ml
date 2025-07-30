2fca373f185d15be5d8d2ac901bd8cfaa2758d60
Mauray-Jain <maurayjain@gmail.com>
2025-07-30 23:11:40 +0530

    part 4: environments

---
+++ lib/env.ml
@@ -0,0 +1,12 @@
+open Types
+
+exception NotFound of string;;
+
+let rec lookup (n, e) =
+    match e with
+    | Nil -> raise (NotFound n)
+    | Pair(Pair(Symbol n', v), rst) ->
+        if n = n' then v else lookup (n, rst)
+    | _ -> raise ThisCan'tHappenError
+
+let bind (n, v, e) = Pair(Pair(n, v), e)
